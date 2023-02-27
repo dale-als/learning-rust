cb407dca9a5783f4420a82f118988445b0d7daef
d.alexandrov <d.alexandrov@xsolla.com>
2023-02-27 18:49:08 +0300

    collections

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod garden;
-mod restaraunt;
-
-use crate::garden::vegetables::Asparagus;
-
-
-
 fn main() {
-    let plant = Asparagus {};
-    println!("I'm growing {:?}!", plant);
-    println!("{}", restaraunt::eat_at_restaraunt());
+    
 }
