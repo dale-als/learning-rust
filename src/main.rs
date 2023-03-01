9e8518942547dc966cca1c66c24ebc9fa0550613
d.aleksandrov <norrow@yandex.ru>
2023-03-01 22:53:13 +0300

    practice

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,23 @@
 use std::collections::HashMap;
+use std::io;
 
 fn main() {
-    let int_list = [1, 1, 2, 3, 4, 5, 4, 6, 8, 4];
-    let mut vector: Vec<i32> = int_list.to_vec();
+    let mut command = String::new();
 
-    
+    println!("Enter your command");
 
-    // vector.sort(); -- usable if vector elements type implement cmp::Ord trait
+    io::stdin()
+        .read_line(&mut command)
+        .expect("Failed to read line");
 
-    for int in &vector {
-        // println!("{int}");
-    }
-    
-    println!("Length: {}", vector.len());    
-    println!("Median: {}", find_median(&vector));
-    println!("Mode: {}", find_mode(&vector));
-}
+    println!("Echo: {command}");
 
-fn find_median(int_vector: &Vec<i32>) -> i32 {
-    let mut vector_copy = int_vector.clone();
+    let mut departments = HashMap::new();
+    let mut accounting = vec!["Alice", "Bob"];
+    let mut sales = vec!["John", "Rick"];
 
-    vector_copy.sort();
+    departments.insert(String::from("Accounting"), accounting);
+    departments.insert(String::from("Accounting"), accounting);
 
-    let middle: f32 = vector_copy.len() as f32 / 2.0;
-    
-    // rounding tests
-    // let ceil = middle.ceil() as usize;
-    let floor = middle.floor() as usize;
-    let middle_value = &vector_copy[floor];
-
-    *middle_value
-}
-
-fn find_mode(int_vector: &Vec<i32>) -> i32 {
-    if int_vector.len() == 0 {
-        return 0;
-    }
-
-    let mut max_count = 0;
-    let mut mode = 0;
-    let mut occurences = HashMap::new();
-    
-    for int in int_vector {
-        let count = occurences.entry(int).or_insert(0);
-        *count += 1;
-    }
-
-    for (_key, value) in &occurences {
-        if *value > max_count {
-            max_count = *value;
-        }
-    }
-
-    for (key, value) in occurences {
-        if value == max_count {
-            mode = *key;
-            break;
-        }
-    }
-
-    mode
+    // let score = departments.get("Accounting").copied().unwrap_or(0);
 }
